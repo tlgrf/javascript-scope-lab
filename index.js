35bca7f76b78f75d40b66cafdcb113a216d8b469
Ridge <ridge.telegrafi@icloud.com>
2025-05-16 23:36:53 -0400

    Initialized burgers array and featuredDrink; added addBurger and changeFeaturedDrink functions; checked work with console.log

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+console.log(burgers)
+console.log(featuredDrink)
+function addBurger() {
+  
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
+  
+  if (true) {
+    const anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+  }
+}
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
+addBurger();
+changeFeaturedDrink ();
+console.log(burgers)
+console.log(featuredDrink)
